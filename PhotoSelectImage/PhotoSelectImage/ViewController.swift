21256acad928b61c4bd384a7daefce5da78415c5
yuhaifei <1129639939@qq.com>
2017-02-08 10:48:54 +0800

    a

---
+++ PhotoSelectImage/PhotoSelectImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoSelectImage
+//
+//  Created by 虞海飞 on 2017/2/8.
+//  Copyright © 2017年 虞海飞. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
