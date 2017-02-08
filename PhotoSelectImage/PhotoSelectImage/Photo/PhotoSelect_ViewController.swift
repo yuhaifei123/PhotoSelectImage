d7f9f50e88c52573de90f5af55345855869d4d50
yuhaifei <1129639939@qq.com>
2017-02-08 10:59:09 +0800

    a

---
+++ PhotoSelectImage/PhotoSelectImage/Photo/PhotoSelect_ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoSelect_ViewController.swift
+//  PhotoSelectImage
+//
+//  Created by 虞海飞 on 2017/2/8.
+//  Copyright © 2017年 虞海飞. All rights reserved.
+//
+
+import UIKit
+
+class PhotoSelect_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
